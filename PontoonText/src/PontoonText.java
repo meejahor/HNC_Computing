ed591905193621b9b59c3f9b585054b8c80ea6c5
Andrew <meejahor@gmail.com>
2020-10-20 14:45:52 +0100

    added comments, corrected local variable names

--- PontoonText/src/PontoonText.java
+++ PontoonText/src/PontoonText.java
@@ -3,36 +3,55 @@ import java.util.Scanner;
 
 public class PontoonText {
 
+    // we need to initialise a Random object to generate random numbers
+    // private = only members of this class can access the object
+    // static = even if we create multiple copies of the class, we'll only have one of these objects
+    // final = once it's assigned (which we do here) it can't be changed
     private static final Random m_Random = new Random();
 
+    // function to generate a random number between 1 and 10 inclusive, and return it
     private static int DrawCard() {
         // nextInt(10) gives a value in the range 0-9 so we need to add 1 to get it in the range 1-10
         return m_Random.nextInt(10) + 1;
     }
 
+    // program entry point
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        int m_PlayerScore = 0;
-        int m_OpponentScore = 18;
+
+        // Scanner object for reading keyboards
+        final Scanner keyboard = new Scanner(System.in);
+
+        int playerScore = 0;
+        int opponentScore = 18; // the assignment said to always give the opponent a score of 18
         int newCardValue;
 
+        // start of repeating loop, see 'while' statement below for end point of loop
         do {
+            // get the new card value
             newCardValue = DrawCard();
+            // tell the player what it was
             System.out.println("You drew card: " + newCardValue);
-            m_PlayerScore += newCardValue;
-            System.out.println("Current score: " + m_PlayerScore);
-            if (m_PlayerScore > 21) {
+            // add it to the player's current score
+            playerScore += newCardValue;
+            // tell the player their current score
+            System.out.println("Current score: " + playerScore);
+            // check if they've gone bust
+            if (playerScore > 21) {
                 System.out.println("You went bust!");
+                // end the game
                 return;
             }
             System.out.print("Draw another card? (y/n) ");
+        // get some input from the player and repeat the loop if the first character is 'y'
         } while (keyboard.next().charAt(0) == 'y');
 
-        System.out.println("Opponent score: " + m_OpponentScore);
+        // at this point the player has chosen to 'stick' so tell them the opponent's score
+        System.out.println("Opponent score: " + opponentScore);
 
-        if (m_PlayerScore > m_OpponentScore) {
+        // compare player vs opponent scores to check for win, lose, or draw
+        if (playerScore > opponentScore) {
             System.out.println("You won!");
-        } else if (m_OpponentScore > m_PlayerScore) {
+        } else if (opponentScore > playerScore) {
             System.out.println("Opponent won!");
         } else {
             System.out.println("It's a draw.");
