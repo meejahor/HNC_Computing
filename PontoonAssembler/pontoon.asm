099aae24ccc86f9ae2deba4c576e574c6fa538ed
Andrew <meejahor@gmail.com>
2020-10-22 11:17:30 +0100

    Reading time works

--- PontoonAssembler/pontoon.asm
+++ PontoonAssembler/pontoon.asm
@@ -1,3 +1,4 @@
+default rel
 global _main
 
 section .text
@@ -9,25 +10,61 @@ _main:
     mov rdx, msg.len
     syscall
 
-    mov rax, 0x2000003 ; read
-    mov rdi, 0 ; stdin
-    mov rsi, buf
-    mov rdx, buf.len
-    syscall
+    ; mov rax, 0x2000003 ; read
+    ; mov rdi, 0 ; stdin
+    ; mov rsi, buf
+    ; mov rdx, buf.len
+    ; syscall
 
-    mov rax, 0x2000004 ; write
-    mov rdi, 1 ; stdout
-    mov rsi, buf
-    mov rdx, buf.len
-    syscall
+    ; mov rax, 0x2000004 ; write
+    ; mov rdi, 1 ; stdout
+    ; mov rsi, buf
+    ; mov rdx, buf.len
+    ; syscall
+
+    call Seed
+    ; call Random
 
     mov rax, 0x2000001 ; exit
-    mov rdi, 0
+    mov rdi, [t_val.micro]
     syscall
+
+Seed:
+    mov rax, 0x2000074
+    lea rdi, [t_val]
+    xor rsi, rsi
+    ; lea rsi, [t_zone]
+    ; lea rdx, [t_absolute]
+    syscall
+    ret
+
+; Random:
+;     mov rax
+
 
 section .data
 
 msg:    db      "Press any key:", 10
 .len:   equ     $ - msg
-buf:    db      2 Dup(?)
+buf:    db      2
 .len:   equ     $ - buf
+
+; struc t_val
+;     seconds: resw 1
+; endstruc
+
+t_val:
+    .seconds dq 0
+    .micro dw 0
+t_zone: times 2 dq 0
+t_absolute: dq 0
+rand: dw 0
+
+; struct:
+;     istruc t_val
+;         at seconds, dw 0
+;     iend
+
+; t_val:
+;     seconds:    qword   0
+;     micro:      dword   0
