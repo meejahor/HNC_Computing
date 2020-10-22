f3b6cea9f8ab94a110280bbf214e5eb772741509
Andrew <meejahor@gmail.com>
2020-10-22 22:45:01 +0100

    Done integer to string printing

--- PontoonAssembler/pontoon.asm
+++ PontoonAssembler/pontoon.asm
@@ -29,9 +29,13 @@ _main:
 
     ; get a random number
     call _random
+    mov rax, [val_Score]
+    mov rdx, [rand]
+    add rax, rdx
+    mov [val_Score], rax
     ; and print it
     mov rax, [rand]
-    call _printValue
+    call _printNumber
 
     ; print the "and" before the second random number
     mov rsi, str_And
@@ -40,21 +44,79 @@ _main:
 
     ; get another random number
     call _random
+    mov rax, [val_Score]
+    mov rdx, [rand]
+    add rax, rdx
+    mov [val_Score], rax
     ; and print it
     mov rax, [rand]
-    call _printValue
+    call _printNumber
 
     call _newLine
 
-    ; print the "and" before the second random number
-    mov rsi, str_Total
-    mov rdx, str_Total.len
-    call _print
+    mov rax, [val_Score]
+    call _printNumber
+
+    call _newLine
 
     mov rax, 0x2000001 ; exit
     mov rdi, [rand]
     syscall
     ret
+
+_printNumber:
+    ; decimal divisor
+    mov r8, 100
+    ; will be set to 1 when we find a digit other than 0
+    ; this removes leading zeroes
+    mov r9, 0
+.loop:
+    ; divide ax by divisor
+    xor rdx, rdx
+    mov rcx, r8
+    div rcx
+    ; rdx is the remainder, which we store for later
+    push rdx
+    ; if we've found anything other than 0 then set r9 to 1
+    cmp rax, 0
+    jne .foundDigit
+.checkIfPrinting:
+    ; if r9 is 1 then always print
+    cmp r9, 1
+    je .print
+.divideBy10:
+    ; divide r8 by 10
+    mov rax, r8
+    xor rdx, rdx
+    mov rcx, 10
+    div rcx
+    mov r8, rax
+    ; retrieve the remainder
+    pop rdx
+    ; and put it back in rax
+    mov rax, rdx
+    ; if r8 is greater than 0 then we've still got decimal places to check
+    cmp r8, 0
+    jg .loop
+    ; done
+    ret
+.foundDigit:
+    ; when r9 is set to 1 we know to always print digits from now on
+    mov r9, 1
+    jmp .checkIfPrinting
+.print:
+    ; put the ASCII code for 0 in rdx
+    mov rdx, '0'
+    ; and add it to rax
+    add rax, rdx
+    ; rax now contans the ASCII code for our random number
+    ; store it
+    mov [buf], rax
+    ; and print it
+    mov rsi, buf
+    mov rdx, 1
+    call _print
+    jmp .divideBy10
 
 _newLine:
     mov rsi, str_Newline
@@ -136,13 +198,13 @@ str_OpeningHand:        db "Your opening hand is: "
 str_And:                db " and "
 .len: equ               $ - str_And
 
-str_Total:                db "Total: "
+str_Total:              db "Total: "
 .len: equ               $ - str_Total
 
 str_Ten:                db "10"
 str_Newline:            db 10
 
-val_Score: dw 0
+val_Score: dq 0
 
 buf:    db      2
 .len:   equ     $ - buf
