f49240fecfd5309f94ac46788864fc6ed57cb6a6
Andrew <meejahor@gmail.com>
2020-10-23 12:22:43 +0100

    RC1

--- PontoonAssembler/pontoon.asm
+++ PontoonAssembler/pontoon.asm
@@ -1,3 +1,6 @@
+; nasm -fmacho64 pontoon.asm
+; ld -macosx_version_min 10.15 -lSystem -o pontoon pontoon.o -no_pie
+
 default rel
 global _main
 
@@ -29,6 +32,7 @@ _main:
     mov rdx, str_And.len
     call _print
 
+_gameLoop:
     ; get another random number and add it to the player's score
     call _random
     call _addRandomToScore
@@ -39,6 +43,7 @@ _main:
     ; start a new line
     call _newLine
 
+.showCurrentScore:
     ; show current score
     mov rsi, str_CurrentTotal
     mov rdx, str_CurrentTotal.len
@@ -49,10 +54,112 @@ _main:
     ; start a new line
     call _newLine
 
-    ; finished
+    ; check if player has gone bust
+    mov rax, [val_Score]
+    cmp rax, 21
+    jg .playerBust
+
+    ; what to do
+    mov rsi, str_WhatToDo
+    mov rdx, str_WhatToDo.len
+    call _print
+
+    ; get user's choice
+    call _readOneCharacter
+
+    ; flush input buffer
+    ; we need to keep the character we just read
+    mov rax, [buf]
+    push rax
+    call _flushStdin
+
+    ; check if player typed 'd' to draw again
+    pop rax
+    cmp rax, 'd'
+    je _drawNextCard
+
+    ; end of game
+    ; show opponent's score
+    mov rsi, str_OpponentScored
+    mov rdx, str_OpponentScored.len
+    call _print
+    mov rax, 18
+    call _printNumber
+
+    ; start a new line
+    call _newLine
+
+    ; check who won
+    mov rax, [val_Score]
+    cmp rax, 18
+    je .draw
+    jg .playerWon
+    jmp .opponentWon
+
+.draw:
+    mov rsi, str_ItsADraw
+    mov rdx, str_ItsADraw.len
+    call _print
+    jmp .exit
+
+.playerWon:
+    mov rsi, str_YouWon
+    mov rdx, str_YouWon.len
+    call _print
+    jmp .exit
+
+.opponentWon:
+    mov rsi, str_OpponentWon
+    mov rdx, str_OpponentWon.len
+    call _print
+    jmp .exit
+
+.playerBust:
+    mov rsi, str_Bust
+    mov rdx, str_Bust.len
+    call _print
+    jmp .exit
+
+.exit:
     mov rax, 0x2000001 ; exit
     mov rdi, 0
     syscall
+    ret
+
+_drawNextCard:
+    ; get another random number and add it to the player's score
+    call _random
+    call _addRandomToScore
+    ; tell the player what card they just drew
+    mov rsi, str_YouDrew
+    mov rdx, str_YouDrew.len
+    call _print
+    mov rax, [rand]
+    call _printNumber
+    ; start a new line
+    call _newLine
+    ; return to the game loop
+    jmp _gameLoop.showCurrentScore
+
+_readOneCharacter:
+    ; read one character from stdin
+    mov rax, 0x2000003
+    mov rdi, 0
+    mov rsi, buf
+    mov rdx, 1
+    syscall
+    ret
+
+_flushStdin:
+    call _readOneCharacter
+    ; check if we've found the linefeed (ASCII 10)
+    mov rax, [buf]
+    cmp rax, 10
+    ; if we have then we're done
+    je .finished
+    ; otherwise read the next charater
+    jmp _flushStdin
+.finished:
     ret
 
 _addRandomToScore:
@@ -116,12 +223,14 @@ _printNumber:
     jmp .divideBy10
 
 _newLine:
+    ; print a newline character
     mov rsi, str_Newline
     mov rdx, 1
     call _print
     ret
 
 _print:
+    ; print string in rsi of length rdx
     mov rax, 0x2000004
     mov rdi, 1
     syscall
@@ -179,8 +288,34 @@ str_CurrentTotal:
 db "Current total: "
 .len: equ $ - str_CurrentTotal
 
-str_Ten:
-db "10"
+str_WhatToDo:
+db "Do you want to draw another card, or stick? (d = draw, s = stick) "
+.len: equ $ - str_WhatToDo
+
+str_YouDrew:
+db "You drew: "
+.len: equ $ - str_YouDrew
+
+str_Bust:
+db "You went bust!", 10
+.len: equ $ - str_Bust
+
+str_OpponentScored:
+db "Your opponent scored: "
+.len: equ $ - str_OpponentScored
+
+str_YouWon:
+db "You won!", 10
+.len: equ $ - str_YouWon
+
+str_OpponentWon:
+db "Your opponent won!", 10
+.len: equ $ - str_OpponentWon
+
+str_ItsADraw:
+db "It's a draw!", 10
+.len: equ $ - str_ItsADraw
+
 str_Newline:
 db 10
 
